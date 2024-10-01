352c21c9b65d4e8571e9bdce1ed9448b5969a735
Eric Nowels <ericnowels@gmail.com>
2024-10-01 15:42:06 -0600

    Main component and FPO layout styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
 	return (
 		<>
 			<Header />
-			<main>
-				<section>
-					<header>
-						<h1>Little Lemon</h1>
-					</header>
-				</section>
-				<section>
-					<header>
-						<h1>Specials</h1>
-					</header>
-				</section>
-				<section>
-					<header>
-						<h1>Testimonials</h1>
-					</header>
-				</section>
-				<section>
-					<header>
-						<h1>About</h1>
-					</header>
-				</section>
-			</main>
+			<Main />
 			<Footer />
 		</>
 	)
---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+function Main() {
+	return (
+		<main>
+			<section>
+				<header>
+					<h1>Little Lemon</h1>
+				</header>
+			</section>
+			<section>
+				<header>
+					<h1>Specials</h1>
+				</header>
+			</section>
+			<section>
+				<header>
+					<h1>Testimonials</h1>
+				</header>
+			</section>
+			<section>
+				<header>
+					<h1>About</h1>
+				</header>
+			</section>
+		</main>
+	)
+}
+
+export default Main
