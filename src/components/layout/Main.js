d6ad3a7045429f7fbef9a1329d85c82fc8683e3e
Eric Nowels <ericnowels@gmail.com>
2024-10-01 19:39:17 -0600

    basic router

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom'
 import Home from '../screens/Home'
 
 function Main() {
 	return (
 		<main>
-			<Home />
+			{/* <Home /> */}
+			<Outlet />
 		</main>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './reset.css'
+import './App.css'
+import ErrorPage from './components/screens/Error'
+import App from './App'
+import Home from './components/screens/Home'
+import BookingConfirmation from './components/screens/BookingConfirmation'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/confirmed',
+				element: <BookingConfirmation />,
+			},
+		],
+	},
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
