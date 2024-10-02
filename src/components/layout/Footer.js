01dd4d637a73101d9fca50aba471f53f4fbaa57a
Eric Nowels <ericnowels@gmail.com>
2024-10-02 11:47:04 -0600

    Feature/footer styles (#3)
    * footer styles
    
    * adding readme content

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,39 +1,55 @@
+import { NavLink } from 'react-router-dom'
 import navigationConfig from '../../config/navigationConfig'
 
 const Footer = () => {
 	return (
-		<footer className='App-Footer'>
-			<div>
-				<img src='/little-lemon-logo.svg' alt='Little Lemon Logo' />
-				<p>&copy; Little Lemon </p>
-			</div>
-			<nav>
-				<ul>
-					{navigationConfig.main.map((link) => (
-						<li key={link.label}>
-							<a href={link.url} onClick={link?.onClick}>
-								{link.label}
-							</a>
-						</li>
-					))}
-				</ul>
-			</nav>
-			<div>
-				<h2>Get in Touch</h2>
-				<p>+1 000-000-0000</p>
-				<p>123 Some St. Boulder, CO</p>
-			</div>
-			<div>
-				<h2>Follow Us</h2>
-				<ul>
-					{navigationConfig.socials.map((link) => (
-						<li key={link.label}>
-							<a href={link.url} target='_blank' rel='noreferrer'>
-								{link.label}
-							</a>
-						</li>
-					))}
-				</ul>
+		<footer className='global-footer'>
+			<div className='container'>
+				<div className='column'>
+					<img
+						src='/little-lemon-logo.svg'
+						alt='Little Lemon Logo'
+						width='148'
+						height='40'
+					/>
+					<div className='w-full'>
+						<small>&copy; Little Lemon 2024</small>
+					</div>
+				</div>
+
+				<nav className='column'>
+					<ul>
+						{navigationConfig.footer.map((link) => (
+							<li key={link.label}>
+								<NavLink to={link.url}>{link.label}</NavLink>
+							</li>
+						))}
+					</ul>
+				</nav>
+
+				<div className='column'>
+					<h2 className='highlight-text'>Get in Touch</h2>
+					<div>+1 303-123-4321</div>
+					<p>123 Some St. Boulder, CO</p>
+				</div>
+
+				<div className='column'>
+					<h2 className='highlight-text'>Follow Us</h2>
+					<ul>
+						{navigationConfig.socials.map((link) => (
+							<li key={link.label}>
+								<a
+									href={link.url}
+									target='_blank'
+									rel='noreferrer'
+									className='transition-color'
+								>
+									{link.label}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</footer>
 	)
