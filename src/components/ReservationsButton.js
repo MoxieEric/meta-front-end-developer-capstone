2777e46f990cc9cd4c8bc97afbee1f2ed861fc90
Eric Nowels <ericnowels@gmail.com>
2024-10-02 18:28:47 -0600

    hero section

--- src/components/ReservationsButton.js
+++ src/components/ReservationsButton.js
@@ -1,4 +1,9 @@
-const ReservationsButton = ({ title = 'Make a Reservation' }) => {
+import classNames from 'classnames'
+
+const ReservationsButton = ({
+	title = 'Make a Reservation',
+	theme = 'primary',
+}) => {
 	const handleClick = () => {
 		const modal = document.getElementById('booking-modal')
 		modal.showModal()
@@ -7,7 +12,12 @@ const ReservationsButton = ({ title = 'Make a Reservation' }) => {
 		<button
 			type='button'
 			onClick={handleClick}
-			className='rounded-md bg-primary text-white px-4 py-2 font-semibold hover:text-white hover:bg-black transition-colors'
+			className={classNames(
+				'rounded-md bg-primary  px-4 py-2 font-semibold transition-colors hover:text-white hover:bg-black',
+				theme === 'primary'
+					? 'bg-primary text-white'
+					: 'bg-secondary text-black'
+			)}
 		>
 			{title}
 		</button>
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,13 +1,48 @@
 import PageSection from '../layout/PageSection'
+import ReservationsButton from '../ReservationsButton'
 
 const Home = () => {
 	return (
 		<>
 			<PageSection id='hero' background='primary'>
-				<header>
-					<h1>Little Lemon</h1>
-					<h2>Boulder</h2>
-				</header>
+				<div className='flex flex-col md:flex-row items-center justify-center py-8 gap-4'>
+					<div className='max-w-prose flex flex-col gap-2'>
+						<header className='text-white'>
+							<h1 className='display-title leading-10'>
+								Little Lemon
+							</h1>
+							<h2 className='subtitle leading-10'>Boulder</h2>
+						</header>
+						<p className='lead-text text-white pr-4 md:pr-8'>
+							Known for our tapas and the town's best outdoor
+							dining, we've been offering exceptional food at a
+							great value for over 10 years.
+						</p>
+						<div className='pt-4'>
+							<ReservationsButton theme='secondary' />
+						</div>
+					</div>
+					<div className='relative'>
+						<figure>
+							<img
+								src='/little-lemon-restaurant-01.jpg'
+								width='694'
+								height='463'
+								alt='The Little Lemon patio'
+								className='w-full max-w-sm h-auto'
+							/>
+						</figure>
+						<figure>
+							<img
+								src='/little-lemon-restauran-food-01@0.5x.jpg'
+								width='239'
+								height='360'
+								alt='Bruschetta'
+								className='w-full max-w-40 h-auto'
+							/>
+						</figure>
+					</div>
+				</div>
 			</PageSection>
 			<PageSection id='specials' background='white'>
 				<header>
